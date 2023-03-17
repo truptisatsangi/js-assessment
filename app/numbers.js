7c9745827f5c9318a8bf95e4eeb82d198840631b
truptiQL <tsatsangi@qodeleaf.com>
2023-03-17 20:30:56 +0530

    numbers Solution

--- app/numbers.js
+++ app/numbers.js
@@ -1,19 +1,32 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.numbersAnswers = {
-  valueAtBit: function(num, bit) {
-
+  valueAtBit: function (num, bit) {
+    const temp = num.toString(2);
+    return parseInt(temp[temp.length - bit]);
   },
 
-  base10: function(str) {
-
+  base10: function (str) {
+    return parseInt(str, 2);
   },
 
-  convertToBinary: function(num) {
-
+  convertToBinary: function (num) {
+    
+    return (num.toString(2).padStart(8, '0'));
   },
 
-  multiply: function(a, b) {
-
+  multiply: function (a, b) {
+    let la = 0,
+    lb = 0;
+  if (!Number.isInteger(a)) {
+    la = a.toString().split(".")[1].length;
   }
+  if (!Number.isInteger(b)) {
+    lb = b.toString().split(".")[1].length;
+  }
+  
+  const l = Math.max(la, lb);
+  
+  return(parseFloat((a * b).toFixed(l)));
+  },
 };
