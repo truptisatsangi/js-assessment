ac2cf24837ba7c65c4f850faa06a45430cb72deb
truptiQL <tsatsangi@qodeleaf.com>
2023-03-17 20:34:38 +0530

    String Solutions

--- app/strings.js
+++ app/strings.js
@@ -1,15 +1,45 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.stringsAnswers = {
-  reduceString: function(str, amount) {
-
+  reduceString: function (str, amount) {
+    let res = "",
+      count = 1;
+    for (let i = 0; i < str.length; i++) {
+      if (count <= amount) {
+        res = res + str[i];
+      }
+      if (str[i + 1] !== str[i]) {
+        count = 1;
+      } else {
+        count++;
+      }
+    }
+    return res;
   },
 
-  wordWrap: function(str, cols) {
-
+  wordWrap: function (str, cols) {
+    let res = "";
+    let i = 0;
+    while (i < str.length) {
+      if (str[i + cols] === " ") {
+        res = res + str.substr(i, cols) + "\n";
+        i += cols + 1;
+      } else {
+        while (str[i] !== " " && i < str.length) {
+          res += str[i++];
+        }
+        res += "\n";
+        i++;
+      }
+    }
+    return res.trim();
   },
 
-  reverseString: function(str) {
-
-  }
+  reverseString: function (str) {
+    let res = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+      res += str[i];
+    }
+    return res;
+  },
 };
