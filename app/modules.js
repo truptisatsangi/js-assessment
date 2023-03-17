63103c08d1ff329bab3aa8f81a9bbf098693cd9c
truptiQL <tsatsangi@qodeleaf.com>
2023-03-17 20:29:56 +0530

    modules solutions

--- app/modules.js
+++ app/modules.js
@@ -1,7 +1,15 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.modulesAnswers = {
-  createModule: function(str1, str2) {
-
-  }
+  createModule: function (str1, str2) {
+    //cannot use arrow function as arrow function doesnot have their own this
+    return {
+      name: str2,
+      greeting: str1,
+      sayIt: function()  {
+        const res = this.greeting + ", " + this.name;
+        return res;
+      }      
+    };
+  },
 };
