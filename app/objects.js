0a0b1ccf6869828bec5a0626d909e5e70d5eddb5
truptiQL <tsatsangi@qodeleaf.com>
2023-03-17 20:32:30 +0530

    Objects Solution

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+   
+     constructor.prototype.greeting = greeting;
+   
+    return constructor;
   },
 
   iterate: function(obj) {
-
+    const result = [];
+    for(let ob in obj){
+      if(obj.hasOwnProperty(ob)){
+        result.push(`${ob}: ${obj[ob]}`)
+      }
+    }
+    return result;
   }
 };
