f991de8d306298ca4618eb216828b2eb462eb4b8
truptiQL <tsatsangi@qodeleaf.com>
2023-03-19 10:25:37 +0530

    count solutions

--- app/count.js
+++ app/count.js
@@ -1,7 +1,24 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    let current = start;
+    let timeoutId;
 
-  }
+    function printCurrentNumber() {
+      console.log(current);
+      current++;
+
+      if (current <= end) {
+        timeoutId = setTimeout(printCurrentNumber, 100);
+      }
+    }
+
+    printCurrentNumber();
+    return {
+      cancel: function () {
+        clearTimeout(timeoutId);
+      },
+    };
+  },
 };
